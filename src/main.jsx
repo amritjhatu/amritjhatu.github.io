0f1bf66ce5974c22a9c2cc1f7eb4ec12bb448ada
Amrit Jhatu <a_jhatu@hotmail.com>
2025-03-28 11:31:01 -0700

    changes.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "HashRouter";
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
