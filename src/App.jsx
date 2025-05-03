66237763c68a99454bed95e2eb0a67f014d3f6d7
Amrit Jhatu <a_jhatu@hotmail.com>
2025-05-03 01:08:11 -0700

    changes to routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
