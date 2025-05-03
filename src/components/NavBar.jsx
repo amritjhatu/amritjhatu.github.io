47ec2a2c2c9ddcf7770d314d2781447fda6642d5
Amrit Jhatu <a_jhatu@hotmail.com>
2025-05-03 00:59:24 -0700

    changes.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
                     </h1>
                 <ul>
                     <li>
-                        <Link to="/">Projects</Link>
+                        <Link to="/projects">Projects</Link>
                     </li>
                     <li>
                         <Link to="/add">Add Student</Link>
