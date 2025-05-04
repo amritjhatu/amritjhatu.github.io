6d40a0a64341cfb73cf71259aea8d9bfeb1ce5c0
Amrit Jhatu <a_jhatu@hotmail.com>
2025-05-04 15:39:03 -0700

    change.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,9 +8,9 @@ const Footer = () => {
             <div className="container">
                 <p>&copy; {new Date().getFullYear()} Amrit Jhatu. All rights reserved.</p>
                 <p>
-                    <a href="https://github.com/amritjhatu" target="_blank" rel="noopener noreferrer">GitHub
+                    <a href="https://github.com/amritjhatu" target="_blank" rel="noopener noreferrer">
                     <FaGithub size={20} style={{ marginRight: '8px' }} /></a> | 
-                    <a href="https://linkedin.com/in/amritjhatu" target="_blank" rel="noopener noreferrer"> LinkedIn
+                    <a href="https://linkedin.com/in/amritjhatu" target="_blank" rel="noopener noreferrer"> 
                     <FaLinkedin size={20} style={{ marginRight: '8px' }} /></a>
                 </p>
             </div>
